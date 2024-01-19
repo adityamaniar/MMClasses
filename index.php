df4aed9dfe2eeed386ae8d872d423d0301f915ee
adityamaniar <adityamaniar240196@gmail.com>
2024-01-19 15:18:33 +0530

    Added favicon

--- index.php
+++ index.php
@@ -7,6 +7,9 @@
     <title>Manisha Maniar Classes</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <link rel="apple-touch-icon" sizes="180x180" href="images/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="images/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="images/favicon-16x16.png">
     <link href="layout/styles/layout.css" rel="stylesheet" type="text/css" media="all">
     <link href="http://www.jqueryscript.net/css/jquerysctipttop.css" rel="stylesheet" type="text/css">
 </head>
